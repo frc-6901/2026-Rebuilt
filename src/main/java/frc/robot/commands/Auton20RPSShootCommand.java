85b445f49acda71a961f90fc9eb147fa32079185
Knight Robotics <frc6901@gmail.com>
2026-03-14 16:39:26 -0500

    added back the preset RPS command someone added but this time as a new command

---
+++ src/main/java/frc/robot/commands/Auton20RPSShootCommand.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class Auton20RPSShootCommand extends Command {
+    private CommandSwerveDrivetrain drivetrain;
+    private ShooterSubsystem shooter;
+
+    public Auton20RPSShootCommand(ShooterSubsystem shooter) {
+        this.shooter = shooter;
+
+        addRequirements(shooter);
+    }
+
+    @Override
+    public void execute() {
+        shooter.shoot(20);
+    }
+}
